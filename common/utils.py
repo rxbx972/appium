4a4e4fb53cefceb93d9a66af5fbe00bd8f39c07a
rxbx <rxbx972@gmail.com>
2025-02-20 18:27:06 +0900

    폴더 정리

---
+++ common/utils.py
@@ -0,0 +1,13 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+
+# 파일 경로 지정
+with open('config/youtubeConfig.json', 'r') as file:
+    config = json.load(file)
+
+# 앱을 종료하고 다시 시작합니다.
+def restart_app(driver):
+    driver.terminate_app(config.app_package)
+    driver.activate_app(config.app_package)
--- tests/test1.py
+++ tests/test1.py
@@ -92,6 +92,52 @@ class TestAppium(unittest.TestCase):
         element.click()
         time.sleep(1)
 
+class SampleTest(unittest.TestCase):
+
+    # 테스트 실행 전 수행해야하는 코드 작성
+    def setUp(self):
+        self.driver = webdriver.Remote(
+            appium_server_url,
+            # capabilities
+            options=UiAutomator2Options().load_capabilities(capabilities)
+        )
+
+    def test_case_1(self):
+        driver = self.driver
+        print("hello")
+        # wait = WebDriverWait(driver, 20)
+
+        # element = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='Media')
+        # element.click()
+
+        # el = driver.find_element(by=AppiumBy.XPATH, value='//*[@text="Battery"]')
+        # el.click()
+
+        # el1 = wait.until(EC.element_to_be_clickable((By.ID, 'com.kakaogame.sample:id/login_ui_button')))
+        # el1.click()
+
+        # el2 = driver.find_element(by=AppiumBy.ID, value="com.kakaogame.sample:id/kakao_game_login_idp_item")
+        # el2.click()
+        # sleep(5)
+
+        # # you could see the failure.
+        # idpCode = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.ListView/android.widget.FrameLayout[4]/android.widget.TextView")
+        # self.assertEqual('iPad', idpCode.get_attribute('text'))
+        # sleep(5)
+
+        # el3 = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.ListView/android.widget.FrameLayout[6]/android.widget.Button")
+        # el3.click()
+        # sleep(5)
+
+        # el4 = driver.find_element(by=AppiumBy.ID, value="com.kakaogame.sample:id/kakao_game_sdk_logout_btn_ok")
+        # el4.click()
+        # sleep(10)
+
+    # 테스트 종료 후 수행해야하는 코드 작성
+    def tearDown(self):
+        if self.driver:
+            self.driver.quit()
+
 if __name__ == '__main__':
     unittest.main()
 
