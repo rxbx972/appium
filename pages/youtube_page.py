a204aba89ea9c780f047cb34bd2a97807f7dc81f
rxbx <rxbx972@gmail.com>
2025-03-05 20:24:49 +0900

    비디오 공유 테스트 추가

--- pages/youtube_page.py
+++ pages/youtube_page.py
@@ -1,6 +1,9 @@
+import time
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 
 class YouTubePage:
     def __init__(self, driver):
@@ -9,9 +12,13 @@ class YouTubePage:
 
     def go_back(self):
         self.driver.press_keycode(4)
+        print("* 뒤로가기")
 
     def allowPermission(self):
-        self.driver.find_element(by=AppiumBy.ID, value="com.android.permissioncontroller:id/permission_allow_button").click()
+        self.driver.find_element(
+            by=AppiumBy.ID,
+            value="com.android.permissioncontroller:id/permission_allow_button",
+        ).click()
 
     def click_shorts_tab(self):
         self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Shorts").click()
@@ -25,62 +32,62 @@ class YouTubePage:
         self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Search").click()
 
     def search_channel(self, query):
-        search_box = self.driver.find_element(
+        self.driver.find_element(
             by=AppiumBy.ID, value="com.google.android.youtube:id/search_edit_text"
-        )
-        search_box.send_keys(query)
+        ).send_keys(query)
         self.driver.press_keycode(66)
 
     def click_view_channel(self):
-        element = self.wait.until(
-            EC.presence_of_element_located(
-                (
-                    AppiumBy.XPATH,
-                    "//android.view.ViewGroup[@content-desc='View Channel']"
-                )
-            )
+        time.sleep(1)
+        viewChannelButton = self.wait.until(
+            EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, "View Channel"))
         )
-        element.click()
+        viewChannelButton.click()
 
     def click_channel_tab(self, tab):
         self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value=tab).click()
 
-    def click_first_video_home(self):
-        self.driver.find_element(AppiumBy.XPATH, "//*[contains(@content-desc, 'play video')][1]").click()
-
-    def click_first_video_videos(self):
+    def click_first_video(self):
         self.driver.find_element(
-            AppiumBy.ID, "com.google.android.youtube:id/results"
-        ).find_element(
             AppiumBy.XPATH, "//*[contains(@content-desc, 'play video')][1]"
         ).click()
+        print("* 첫번째 비디오 클릭")
 
     def share_video(self):
-        print(self.driver.page_source)
-        self.driver.find_element(
-            by=AppiumBy.ACCESSIBILITY_ID, value="Share"
-        ).click()
-        self.driver.find_element(
-            by=AppiumBy.XPATH, value="//android.support.v7.widget.RecyclerView[@resource-id='com.google.android.youtube:id/bottom_sheet_list']/android.view.ViewGroup/android.view.ViewGroup[2]"
-        ).click()
-        # shareButton = self.wait.until(
-        #     EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, "Share"))
-        # )
-        # shareButton.click()
-        # copyLinkButton = self.wait.until(
-        #     EC.presence_of_element_located(
-        #         (
-        #             AppiumBy.XPATH,
-        #             "//android.support.v7.widget.RecyclerView[@resource-id='com.google.android.youtube:id/bottom_sheet_list']/android.view.ViewGroup/android.view.ViewGroup[2]",
-        #         )
-        #     )
-        # )
-        # copyLinkButton.click()
+        time.sleep(2)
+        try:
+            shareButton = self.wait.until(
+                EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, "Share"))
+            )
+            shareButton.click()
+            print("* 비디오 공유 버튼 클릭")
+        except TimeoutException:
+            skipButton = self.wait.until(
+                EC.presence_of_element_located(
+                    (AppiumBy.ID, "com.google.android.youtube:id/skip_ad_button")
+                )
+            )
+            skipButton.click()
+
+            shareButton = self.wait.until(
+                EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, "Share"))
+            )
+            shareButton.click()
+            print("* 광고 스킵 후 비디오 공유 버튼 클릭")
+        finally:
+            copyLinkButton = self.wait.until(
+                EC.presence_of_element_located(
+                    (
+                        AppiumBy.XPATH,
+                        "//android.support.v7.widget.RecyclerView[@resource-id='com.google.android.youtube:id/bottom_sheet_list']/android.view.ViewGroup/android.view.ViewGroup[2]",
+                    )
+                )
+            )
+            copyLinkButton.click()
+            print("* 링크 복사 버튼 클릭")
 
     def close_miniplayer(self):
         self.driver.find_element(
             by=AppiumBy.ACCESSIBILITY_ID, value="Close miniplayer"
         ).click()
-
-
-
+        print("* 미니 플레이어 닫기")
